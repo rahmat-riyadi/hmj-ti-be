2415d77a2f6a66a87ca113de3a98126220f75d2c
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-03-07 19:20:51 +0700

    a

--- app/Http/Controllers/API/ResponseController.php
+++ app/Http/Controllers/API/ResponseController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class ResponseController extends Controller
 {
-    //
+    static function create($data = [], $status, $message, $code){
+        $data = [
+            'code' => $code,
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ];
+        return response()->json($data, $code);   
+    }
 }
--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,13 +6,5 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    static function create($data = [], $status, $message, $code){
-        $data = [
-            'code' => $code,
-            'status' => $status,
-            'message' => $message,
-            'data' => $data
-        ];
-        return response()->json($data, $code);   
-    }
+    
 }
