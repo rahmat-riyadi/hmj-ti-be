9ef73a647ec54eec5f43e6b246355c5fa713362d
Rahmat Riyadi <rahmat-riyadi@Rahmat-Riyadi-Macbook-Pro.local>
2023-03-07 19:20:27 +0800

    feat: response format

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    static function create($data = [], $status, $message, $code){
+        $data = [
+            'code' => $code,
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ];
+        return response()->json($data, $code);   
+    }
 }
